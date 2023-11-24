3a92c2aeed2ab259e523a2a81d638a30a929aabf
NikFri94 <139549862+NikFri94@users.noreply.github.com>
2023-11-24 12:16:06 +0100

    Upload

---
+++ app/src/main.cpp
@@ -0,0 +1,16 @@
+/*!
+ *
+ *  @file      main.cpp
+ *  @brief     Haupteinstiegspunkt der ET2-App (Desktopanwendung)
+ *  @details   ~
+ *  @author    XXX
+ *  @date      24.11.2023
+ *  @copyright © XXX, 2023. All right reserved.
+ *
+ */
+
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
